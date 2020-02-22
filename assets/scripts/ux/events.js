09d6c9297187c782194dea49275c1d276b48047e
srsexton94 <srsexton94@gmail.com>
2020-02-22 18:05:07 -0500

    Adds descriptive message to form selections
    Adds empty <p> element and function logic (in ux/events) to post
    a confirmation message (make sure users understand what their emoji
    selection means)

--- assets/scripts/ux/events.js
+++ assets/scripts/ux/events.js
@@ -4,8 +4,14 @@ const hoverDescribe = event => {
   // console.log(event.target)
 }
 
+const selectRadio = event => {
+  const myMood = event.target.id + '?'
+  $('#mood-message').text(myMood)
+}
+
 const addHandlers = () => {
   $('.mood').on('mouseover', hoverDescribe)
+  $('.radio').on('click', selectRadio)
 }
 
 module.exports = {

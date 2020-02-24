46430d87061ed880886d1f36d0190596caeef9b2
srsexton94 <srsexton94@gmail.com>
2020-02-24 08:47:47 -0500

    Corrects typo error in config URL
    Removes extra '/' at end of production & development URLs in
    assets/config.js in attempt at resolveing request error
    API requests still throwing error

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,8 +2,8 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://moodtracker-api.herokuapp.com/',
-  development: 'http://localhost:4741/'
+  production: 'https://moodtracker-api.herokuapp.com',
+  development: 'http://localhost:4741'
 }
 
 if (window.location.hostname === 'localhost') {

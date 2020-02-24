e4a25a0a416a57dc016e82efe39af0eea4ce31ea
srsexton94 <srsexton94@gmail.com>
2020-02-24 15:51:04 -0500

    Edits setTimeout/message clears for form messages

--- assets/scripts/resources/ui.js
+++ assets/scripts/resources/ui.js
@@ -1,14 +1,19 @@
 'use strict'
 
 const onSubmitFormSuccess = () => {
-  $('#form-message').text('You submitted successfully!')
+  $('#mood-message').text('')
+  $('#form-message').text('You submitted successfully!').css('color', 'green')
+
+  setTimeout(() => {
+    $('#form-message').text('') // clears the failure message after 2 seconds
+  }, 2000)
 }
 
 const onSubmitFormFailure = () => {
   $('#form-message').text('There was an error, please try again later.').css('color', 'red')
 
   setTimeout(() => {
-    $('#form-message').text('') // clears the failure message after 5 seconds
+    $('#form-message').text('') // clears the failure message after 3 seconds
   }, 3000)
 }
 

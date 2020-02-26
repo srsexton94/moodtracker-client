4b2d2047bcaf66a95e41d20565523a57e0c34f54
srsexton94 <srsexton94@gmail.com>
2020-02-25 21:53:32 -0500

    Makes a Hamburger menu bar for mobile views
    Makes a "hamburger" button to expand and collapse the navigation
    bar in mobile views
    (Media query not yet added so it applies to browser too for now)

--- assets/scripts/ux/events.js
+++ assets/scripts/ux/events.js
@@ -1,5 +1,17 @@
 'use strict'
 
+const openMobileNav = event => {
+  $('#mobilenav-open').css('display', 'none')
+  $('#mobilenav').css('display', 'block')
+  $('#mobilenav-close').css('display', 'inline-block')
+}
+
+const closeMobileNav = event => {
+  $('#mobilenav').css('display', 'none')
+  $('#mobilenav-close').css('display', 'none')
+  $('#mobilenav-open').css('display', 'block')
+}
+
 const selectRadio = event => {
   const myMood = event.target.id + '?'
   $('#mood-message').text(myMood)
@@ -7,6 +19,8 @@ const selectRadio = event => {
 
 const addHandlers = () => {
   $('.radio').on('click', selectRadio)
+  $('#mobilenav-open').on('click', openMobileNav)
+  $('#mobilenav-close').on('click', closeMobileNav)
 }
 
 module.exports = {
